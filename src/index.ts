2f3dd288381e8b29b18416e02320db8d0fe2fd29
Nicholas Cronquist <nick@ncronquist.com>
2021-02-25 23:22:35 -0500

    Very in-progress...

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,57 @@
 
 import {Command, flags} from '@oclif/command'
 import * as puppeteer from 'puppeteer'
+import {URL} from 'url';
 
 const sleep = (waitTimeInMs: number) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+function parsePhotoID(photoURL: string): string {
+  const pURL = new URL(photoURL);
+  return pURL.searchParams.get('fbid');
+}
+
+async function downloadPhotos(page: puppeteer.Page, photoURL: string): void {
+  const photoID = parsePhotoID(photoURL)
+  console.log('PHOTO ID:', photoID);
+
+  // await page.goto(photoURL, {waitUntil: "networkidle0"});
+  setTimeout(() => {
+    console.log('GOTO URL')
+    page.goto(photoURL)
+  }, 1000); // Delete the page.goto so that the page.waitForResponse has time to start properly
+  console.log('WAIT FOR RESPONSE')
+  const photoResponse = await page.waitForResponse((response: puppeteer.HTTPResponse) => {
+    // console.log('R URL:', response.url());
+    const headers = response.headers();
+    const contentID = headers['x-content-id'];
+    const contentType = headers['content-type'];
+
+    if (contentType === 'image/jpeg') {
+      console.log('CONTENT ID:', contentID);
+      if (contentID.slice(0,12) === photoID.slice(0,12)) {
+        // console.log('RESPONSE HEADERS:', headers)
+        // console.log('RESPONSE URL:', response.url());
+        return true;
+      }
+    }
+
+    return false;
+    // response.headers()['x-conent-id'] === photoID && response.status() === 200);
+  });
+
+  console.log('PHOTO URL:', photoResponse.url());
+
+  let nextPhotoURL = await page.$$eval('div', divs => divs.find(div => div.role === 'button' && div['aria-label'] === "Next photo"));
+  console.log('NEXT PHOTO:', nextPhotoURL);
+  // await page.on('response', response => {
+  //   const headers = response.headers();
+  //   if (headers['x-content-id']) {
+  //     console.log('RESPONSE HEADERS:', headers)
+  //     console.log('RESPONSE URL:', response.url());
+  //   }
+  // })
+  return
+}
 
 class FbPhotoDownloader extends Command {
   static description = 'Downloads your tagged photos from Facebook'
@@ -36,7 +85,10 @@ class FbPhotoDownloader extends Command {
         height: 720,
       }
     });
-    const page = await browser.newPage();
+    const context = await browser.createIncognitoBrowserContext();
+    // Create a new page in a pristine context.
+    const page = await context.newPage();
+    // const page = await browser.newPage();
     await page.goto('https://facebook.com');
     await sleep(20000);
 
@@ -44,13 +96,17 @@ class FbPhotoDownloader extends Command {
 
     // await sleep(10000);
     // let linksNL = await page.$$('a');
-    let firstPhoto = await page.$$eval('a', links => links.find(link => link.href.startsWith("https://www.facebook.com/photo.php?fbid")).href)
+    let firstPhotoUrl = await page.$$eval('a', links => links.find(link => link.href.startsWith("https://www.facebook.com/photo.php?fbid")).href)
     // let links = Array.from(linksNL);
     // console.log('LINKS:', links);
     // let firstPhoto = links.find(link => link.href.startsWith("https://www.facebook.com/photo.php?fbid")).href
-    console.log('FIRST PHOTO:', firstPhoto);
+    console.log('FIRST PHOTO:', firstPhotoUrl);
 
-    await page.goto(firstPhoto, {waitUntil: "networkidle0"});
+    await downloadPhotos(page, firstPhotoUrl)
+
+
+    // const headers = response.headers();
+    // console.log(headers);
 
     // await page.on('domcontentloaded', async () => {
     //   let links = await page.$$('a');
